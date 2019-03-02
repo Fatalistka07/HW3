80fb03026d9e49b69be240e4b531f7d643d24c73
Olenka <olenka07.07@mail.ru>
2019-03-02 20:34:38 +0300

    Десериализация JSON из ответа от https://randomuser.me/ в массив клиентов.

--- src/main/java/com/company/Client.java
+++ src/main/java/com/company/Client.java
@@ -1,23 +1,126 @@
 package com.company;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Client {
-    public String name;
-    public String surname;
+    public class FullName {
+        public String title;
+        public String first;
+        public String last;
+    }
+
+    public FullName name;
+
+    public String getSurname() {
+        return name.last;
+    }
+
+    public String getName() {
+        return name.first;
+    }
+
+    public String getPatronomic() {
+        return name.title;
+    }
+
     public String patronymic;
-    public String sex;
+    public String gender;
+
+    public String getGender() {
+        return gender;
+    }
+
     public LocalDate birthDay;
-    public String birthDayStr() {return birthDay.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));}
-    public int age() { return Period.between(birthDay, LocalDate.now()).getYears();}
+
+    public class DateAndAge {
+        public Date date;
+        public int age;
+    }
+
+    public int getAge() {
+        return dob.age;
+    }
+
+    public DateAndAge dob;
+
+    public String gatBD() {
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        dateFormat.setLenient(false);
+        return dateFormat.format(dob.date);
+        //return birthDay.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public int age() {
+        return Period.between(birthDay, LocalDate.now()).getYears();
+    }
+
     public long inn;
-    public int index;
-    public String country;
-    public String region;
-    public String city;
-    public String street;
+    public String nat;
+
+    public String getCountry() {
+        return nat;
+    }
+
+    public class Location {
+        public String state;
+        public String city;
+        public String street;
+        public String postcode;
+
+        public class Coordinates {
+            public double latitude;
+            public double longitude;
+        }
+
+        public Coordinates coordinates;
+
+        public class Timezone {
+            public String offset;
+            public String description;
+        }
+
+        public Timezone timezone;
+    }
+
+    public String getRegion() {
+        return location.state;
+    }
+
+    public String getCity() {
+        return location.city;
+    }
+
+    public String getStreet() {
+        Pattern pattern = Pattern.compile("\\D+");
+        Matcher streetMatch = pattern.matcher(location.street);
+        streetMatch.find();
+        return location.street.substring(streetMatch.start(),streetMatch.end());
+    }
+
+    public int getHouse() {
+        String []streetWords = location.street.split(" ");
+        try {
+            return Integer.decode(streetWords[0]);
+
+        }
+        catch (NumberFormatException ex){
+            return Integer.decode(streetWords[streetWords.length - 1]);
+
+        }
+    }
+
+    public String getIndex() {
+        return location.postcode;
+    }
+
+    public Location location;
     public int home;
     public int flat;
 }
--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -9,6 +9,10 @@ import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
@@ -26,32 +30,46 @@ import static org.apache.http.HttpHeaders.USER_AGENT;
 public class Main {
 
     public static final String ExcelFileName = "Clients.xls";
-    public static final String PdfFileName = "Clients.pdf";
 
     public static void main(String[] args) throws IOException, URISyntaxException {
         Random r = new Random();
         int n = 1 + r.nextInt(30);
-requestClients(n);
-        Client[] clients = generateClients(r, n);
+        Client[] clients;
+        clients = requestClients(n);
+        // clients = generateClients(r, n);
 
-        //generateExcel(clients);
-
-        //generatePdf(clients);
+        generateExcel(clients);
     }
 
-    private static void requestClients(int n) throws URISyntaxException, IOException {
+    private static Client[] requestClients(int n) throws URISyntaxException, IOException {
         HttpClient client = HttpClientBuilder.create().build();
         URIBuilder uriBuilder = new URIBuilder("https://randomuser.me/api/");
         uriBuilder.setParameter("results", Integer.toString(n));
+        uriBuilder.setParameter("inc", "name,dob,location,gender,nat");
         HttpGet request = new HttpGet(uriBuilder.build());
         HttpResponse response = client.execute(request);
         BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-        String line = "";
+        String line;
         StringBuffer result = new StringBuffer();
         while ((line = reader.readLine()) != null) {
             result.append(line);
         }
-        System.out.println(result.toString());
+        Client [] clients = new Client[n];
+        JSONObject jsonObject = new JSONObject(result.toString());
+        JSONArray arrResults = jsonObject.getJSONArray("results");
+        ObjectMapper mapper = new ObjectMapper();
+        Random r = new Random();
+        for (int i = 0; i < arrResults.length(); i++)
+        {
+            JSONObject jsonClient = arrResults.getJSONObject(i);
+            jsonClient.toString();
+            clients[i] = mapper.readValue(jsonClient.toString(), Client.class);
+            clients[i].inn = getRandInn(r);
+            clients[i].flat = 1 + r.nextInt(200);
+        }
+        return clients;
+
+
     }
 
     private static Client[] generateClients(Random r, int n) throws IOException {
@@ -70,24 +88,24 @@ requestClients(n);
         for (int i = 0; i < n; i++) {
             clients[i] = new Client();
             if (r.nextBoolean()) {
-                clients[i].sex = "м";
-                clients[i].name = manNames.get(r.nextInt(manNames.size()));
-                clients[i].surname = manSurnames.get(r.nextInt(manSurnames.size()));
-                clients[i].patronymic = manPatronymics.get(r.nextInt(manPatronymics.size()));
+                clients[i].gender = "м";
+                clients[i].name.first = manNames.get(r.nextInt(manNames.size()));
+                clients[i].name.last = manSurnames.get(r.nextInt(manSurnames.size()));
+                clients[i].name.title = manPatronymics.get(r.nextInt(manPatronymics.size()));
             } else {
-                clients[i].sex = "ж";
-                clients[i].name = womenNames.get(r.nextInt(manNames.size()));
-                clients[i].surname = womenSurnames.get(r.nextInt(manSurnames.size()));
-                clients[i].patronymic = womenPatronymics.get(r.nextInt(manPatronymics.size()));
+                clients[i].gender = "ж";
+                clients[i].name.first = womenNames.get(r.nextInt(manNames.size()));
+                clients[i].name.last = womenSurnames.get(r.nextInt(manSurnames.size()));
+                clients[i].name.title = womenPatronymics.get(r.nextInt(manPatronymics.size()));
             }
 
             clients[i].birthDay = getRandBD(r);
             clients[i].inn = getRandInn(r);
-            clients[i].index = 100000 + r.nextInt(100000);
-            clients[i].country = countries.get(r.nextInt(countries.size()));
-            clients[i].region = regions.get(r.nextInt(regions.size()));
-            clients[i].city = cities.get(r.nextInt(cities.size()));
-            clients[i].street = streets.get(r.nextInt(streets.size()));
+            clients[i].location.postcode = Integer.toString(100000 + r.nextInt(100000));
+            clients[i].nat = countries.get(r.nextInt(countries.size()));
+            clients[i].location.state = regions.get(r.nextInt(regions.size()));
+            clients[i].location.city = cities.get(r.nextInt(cities.size()));
+            clients[i].location.street = streets.get(r.nextInt(streets.size()));
             clients[i].home = 1 + r.nextInt(100);
             clients[i].flat = 1 + r.nextInt(200);
         }
@@ -170,21 +188,21 @@ requestClients(n);
         for (int rowIx = 1; rowIx <= clients.length; rowIx++) {
             Row row = sheet.createRow(rowIx);
             colIx = 0;
-            row.createCell(colIx++).setCellValue(clients[rowIx-1].name);
-            row.createCell(colIx++).setCellValue(clients[rowIx-1].surname);
-            row.createCell(colIx++).setCellValue(clients[rowIx-1].patronymic);
-            row.createCell(colIx++).setCellValue(clients[rowIx-1].age());
-            row.createCell(colIx++).setCellValue(clients[rowIx-1].sex);
-            row.createCell(colIx++).setCellValue(clients[rowIx-1].birthDayStr());
+            row.createCell(colIx++).setCellValue(clients[rowIx-1].getName());
+            row.createCell(colIx++).setCellValue(clients[rowIx-1].getSurname());
+            row.createCell(colIx++).setCellValue(clients[rowIx-1].getPatronomic());
+            row.createCell(colIx++).setCellValue(clients[rowIx-1].getAge());
+            row.createCell(colIx++).setCellValue(clients[rowIx-1].getGender());
+            row.createCell(colIx++).setCellValue(clients[rowIx-1].gatBD());
             Cell innCell = row.createCell(colIx++);
             innCell.setCellStyle(innStyle);
             innCell.setCellValue(clients[rowIx-1].inn);
-            row.createCell(colIx++).setCellValue(clients[rowIx-1].index);
-            row.createCell(colIx++).setCellValue(clients[rowIx-1].country);
-            row.createCell(colIx++).setCellValue(clients[rowIx-1].region);
-            row.createCell(colIx++).setCellValue(clients[rowIx-1].city);
-            row.createCell(colIx++).setCellValue(clients[rowIx-1].street);
-            row.createCell(colIx++).setCellValue(clients[rowIx-1].home);
+            row.createCell(colIx++).setCellValue(clients[rowIx-1].getIndex());
+            row.createCell(colIx++).setCellValue(clients[rowIx-1].getCountry());
+            row.createCell(colIx++).setCellValue(clients[rowIx-1].getRegion());
+            row.createCell(colIx++).setCellValue(clients[rowIx-1].getCity());
+            row.createCell(colIx++).setCellValue(clients[rowIx-1].getStreet());
+            row.createCell(colIx++).setCellValue(clients[rowIx-1].getHouse());
             row.createCell(colIx++).setCellValue(clients[rowIx-1].flat);
         }
         for (colIx = 0; colIx < 14; colIx++) {
@@ -195,91 +213,4 @@ requestClients(n);
         System.out.println(new File(ExcelFileName).getAbsolutePath());
     }
 
-    private static void generatePdf(Client[] clients) {
-        String xmlStr = generateXml(clients);
-        convertXmlToPdf(xmlStr);
-        System.out.println(new File(PdfFileName).getAbsolutePath());
-    }
-
-    private static String generateXml(Client[] clients) {
-        StringBuffer sb = new StringBuffer();
-        sb.append("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n\n<fo:root xmlns:fo=\"http://www.w3.org/1999/XSL/Format\">\n\n    <fo:layout-master-set>\n        <fo:simple-page-master master-name=\"spm\"\n                               page-height=\"29.7cm\"\n                               page-width=\"21cm\"\n                               margin-top=\"1cm\"\n                               margin-bottom=\"1cm\"\n                               margin-left=\"1cm\"\n                               margin-right=\"1cm\">\n            <fo:region-body/>\n        </fo:simple-page-master>\n    </fo:layout-master-set>\n\n    <fo:page-sequence master-reference=\"spm\">\n        <fo:flow flow-name=\"xsl-region-body\">\n\n" +
-                "<fo:table table-layout=\"fixed\" width=\"100%\" border-collapse=\"separate\">\n" +
-                "   <fo:table-column column-width=\"12mm\"/>\n" +
-                "   <fo:table-column column-width=\"19mm\"/>\n" +
-                "   <fo:table-column column-width=\"19mm\"/>\n" +
-                "   <fo:table-column column-width=\"7mm\"/>\n" +
-                "   <fo:table-column column-width=\"4mm\"/>\n" +
-                "   <fo:table-column column-width=\"15mm\"/>\n" +
-                "   <fo:table-column column-width=\"19mm\"/>\n" +
-                "   <fo:table-column column-width=\"10mm\"/>\n" +
-                "   <fo:table-column column-width=\"15mm\"/>\n" +
-                "   <fo:table-column column-width=\"19mm\"/>\n" +
-                "   <fo:table-column column-width=\"19mm\"/>\n" +
-                "   <fo:table-column column-width=\"19mm\"/>\n" +
-                "   <fo:table-column column-width=\"5mm\"/>\n" +
-                "   <fo:table-column column-width=\"5mm\"/>\n" +
-                " <fo:table-body>");
-
-        // table header
-        sb.append("<fo:table-row>\n");
-        xmlAddCell(sb, "Имя");
-        xmlAddCell(sb, "Фамилия");
-        xmlAddCell(sb, "Отчество");
-        xmlAddCell(sb, "Возраст");
-        xmlAddCell(sb, "Пол");
-        xmlAddCell(sb, "Дата рождения");
-        xmlAddCell(sb, "ИНН");
-        xmlAddCell(sb, "Индекс");
-        xmlAddCell(sb, "Страна");
-        xmlAddCell(sb, "Область");
-        xmlAddCell(sb, "Город");
-        xmlAddCell(sb, "Улица");
-        xmlAddCell(sb, "Дом");
-        xmlAddCell(sb, "Кв.");
-        sb.append("</fo:table-row>\n");
-
-        for (int i = 0; i < clients.length; i++){
-            sb.append("<fo:table-row>\n");
-            xmlAddCell(sb, clients[i].name);
-            xmlAddCell(sb, clients[i].surname);
-            xmlAddCell(sb, clients[i].patronymic);
-            xmlAddCell(sb, Integer.toString(clients[i].age()));
-            xmlAddCell(sb, clients[i].sex);
-            xmlAddCell(sb, clients[i].birthDayStr());
-            xmlAddCell(sb, Long.toString(clients[i].inn));
-            xmlAddCell(sb, Integer.toString(clients[i].index));
-            xmlAddCell(sb, clients[i].country);
-            xmlAddCell(sb, clients[i].region);
-            xmlAddCell(sb, clients[i].city);
-            xmlAddCell(sb, clients[i].street);
-            xmlAddCell(sb, Integer.toString(clients[i].home));
-            xmlAddCell(sb, Integer.toString(clients[i].flat));
-            sb.append("</fo:table-row>\n");
-        }
-        sb.append("</fo:table-body>\n </fo:table>\n </fo:flow>\n </fo:page-sequence>\n </fo:root>");
-        return sb.toString();
-    }
-
-    private static void xmlAddCell(StringBuffer sb, String cellValue) {
-        sb.append("<fo:table-cell> <fo:block font-family=\"Arial\" font-size=\"6pt\"> \n");
-        sb.append(cellValue);
-        sb.append("</fo:block>\n </fo:table-cell>\n");
-    }
-
-    private static void convertXmlToPdf(String xmlStr){
-        FopFactory fopFactory = FopFactory.newInstance();
-        TransformerFactory transformerFactory = TransformerFactory.newInstance();
-
-        try {
-            Fop fop = fopFactory.newFop("application/pdf", new FileOutputStream(PdfFileName));
-            Transformer transformer = transformerFactory.newTransformer();
-            Source source = new StreamSource(new ByteArrayInputStream(xmlStr.getBytes("UTF8")));
-            Result result = new SAXResult(fop.getDefaultHandler());
-            transformer.transform(source, result);
-        } catch (Exception var8) {
-            System.err.println("Error of PDf generating: " + var8.getLocalizedMessage());
-        }
-
-    }
 }
