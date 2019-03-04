62a781676538d2abacdf642e0457ec574e008c50
Olenka <olenka07.07@mail.ru>
2019-03-04 22:51:54 +0300

    Исправлены Warning-и в pom.xml и добавлена обработка исключений.

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -20,10 +20,10 @@ import java.util.*;
 
 public class Main {
 
-    public static final String ExcelFileName = "Clients.xls";
+    public static final String EXCEL_FILE_NAME = "Clients.xls";
     public static final String RANDOMUSER_ME_API = "https://randomuser.me/api/";
 
-    public static void main(String[] args) throws IOException, URISyntaxException {
+    public static void main(String[] args)  {
         Random r = new Random();
         int n = 1 + r.nextInt(30);
         Client[] clients;
@@ -35,41 +35,47 @@ public class Main {
             System.out.println("Connection to Internet is broken (can't load data from " + RANDOMUSER_ME_API
                     + "). Generating data from local resources.");
             clients = generateClients(r, n);
+        } catch (IOException ioaEx) {
+            System.out.println("Fatal error: "+ ioaEx.getMessage());
+            clients = generateClients(r, n);
         }
 
         generateExcel(clients);
     }
 
-    private static ApiClient[] requestClients(int n) throws URISyntaxException, IOException {
-        HttpClient client = HttpClientBuilder.create().build();
-        URIBuilder uriBuilder = new URIBuilder(RANDOMUSER_ME_API);
-        uriBuilder.setParameter("results", Integer.toString(n));
-        uriBuilder.setParameter("inc", "name,dob,location,gender,nat");
-        HttpGet request = new HttpGet(uriBuilder.build());
-        HttpResponse response = client.execute(request);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-        String line;
-        StringBuffer result = new StringBuffer();
-        while ((line = reader.readLine()) != null) {
-            result.append(line);
-        }
-        ApiClient[] clients = new ApiClient[n];
-        JSONObject jsonObject = new JSONObject(result.toString());
-        JSONArray arrResults = jsonObject.getJSONArray("results");
-        ObjectMapper mapper = new ObjectMapper();
-        Random r = new Random();
-        for (int i = 0; i < arrResults.length(); i++)
-        {
-            JSONObject jsonClient = arrResults.getJSONObject(i);
-            jsonClient.toString();
-            clients[i] = mapper.readValue(jsonClient.toString(), ApiClient.class);
-            clients[i].inn = getRandInn(r);
-            clients[i].flat = 1 + r.nextInt(200);
+    private static ApiClient[] requestClients(int n) throws IOException {
+            ApiClient[] clients = new ApiClient[n];
+        try {
+            HttpClient client = HttpClientBuilder.create().build();
+            URIBuilder uriBuilder = new URIBuilder(RANDOMUSER_ME_API);
+            uriBuilder.setParameter("results", Integer.toString(n));
+            uriBuilder.setParameter("inc", "name,dob,location,gender,nat");
+            HttpGet request = new HttpGet(uriBuilder.build());
+            HttpResponse response = client.execute(request);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            String line;
+            StringBuffer result = new StringBuffer();
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
+            }
+            JSONObject jsonObject = new JSONObject(result.toString());
+            JSONArray arrResults = jsonObject.getJSONArray("results");
+            ObjectMapper mapper = new ObjectMapper();
+            Random r = new Random();
+            for (int i = 0; i < arrResults.length(); i++) {
+                JSONObject jsonClient = arrResults.getJSONObject(i);
+                jsonClient.toString();
+                clients[i] = mapper.readValue(jsonClient.toString(), ApiClient.class);
+                clients[i].inn = getRandInn(r);
+                clients[i].flat = 1 + r.nextInt(200);
+            }
+        } catch (URISyntaxException uriEx) {
+            System.out.println("Fatal error: Failed to build URI.");
         }
         return clients;
     }
 
-    private static TxtGeneratedClient[] generateClients(Random r, int n) throws IOException {
+    private static TxtGeneratedClient[] generateClients(Random r, int n) {
         ArrayList<String> cities = getArrayFromFilePath("./resources/Cities.txt");
         ArrayList<String> countries = getArrayFromFilePath("./resources/Countries.txt");
         ArrayList<String> manNames = getArrayFromFilePath("./resources/ManNames.txt");
@@ -109,15 +115,20 @@ public class Main {
         return clients;
     }
 
-    private static ArrayList<String> getArrayFromFilePath(String filePath) throws IOException {
-        FileInputStream fstream = new FileInputStream(filePath);
-        Scanner scanner = new Scanner(new InputStreamReader(fstream));
+    private static ArrayList<String> getArrayFromFilePath(String filePath) {
         ArrayList<String> result = new ArrayList<String>();
-        while ( scanner.hasNext()) {
-            result.add(scanner.nextLine());
+        try {
+            Scanner scanner = new Scanner(new InputStreamReader(new FileInputStream(filePath)));
+
+            while (scanner.hasNext()) {
+                result.add(scanner.nextLine());
+            }
+        } catch (IOException ex) {
+            System.out.println("Could not read data due to " + ex.getMessage());
         }
         return result;
     }
+
     private static long getRandInn(Random r){
         int inspection = 1 + r.nextInt(36);
         int [] innDigits = {
@@ -159,55 +170,59 @@ public class Main {
         return LocalDate.ofYearDay(year, day);
     }
 
-    private static void generateExcel(Client[] clients) throws IOException {
-        Workbook book = new HSSFWorkbook();
-        Sheet sheet = book.createSheet("Clients");
-        Row titleRow = sheet.createRow(0);
-        int colIx = 0;
-        titleRow.createCell(colIx++).setCellValue("Имя");
-        titleRow.createCell(colIx++).setCellValue("Фамилия");
-        titleRow.createCell(colIx++).setCellValue("Отчество");
-        titleRow.createCell(colIx++).setCellValue("Возраст");
-        titleRow.createCell(colIx++).setCellValue("Пол");
-        titleRow.createCell(colIx++).setCellValue("Дата рождения");
-        titleRow.createCell(colIx++).setCellValue("ИНН");
-        titleRow.createCell(colIx++).setCellValue("Индекс");
-        titleRow.createCell(colIx++).setCellValue("Страна");
-        titleRow.createCell(colIx++).setCellValue("Область");
-        titleRow.createCell(colIx++).setCellValue("Город");
-        titleRow.createCell(colIx++).setCellValue("Улица");
-        titleRow.createCell(colIx++).setCellValue("Дом");
-        titleRow.createCell(colIx++).setCellValue("Кв.");
+    private static void generateExcel(Client[] clients) {
+        try {
+            Workbook book = new HSSFWorkbook();
+            Sheet sheet = book.createSheet("Clients");
+            Row titleRow = sheet.createRow(0);
+            int colIx = 0;
+            titleRow.createCell(colIx++).setCellValue("Имя");
+            titleRow.createCell(colIx++).setCellValue("Фамилия");
+            titleRow.createCell(colIx++).setCellValue("Отчество");
+            titleRow.createCell(colIx++).setCellValue("Возраст");
+            titleRow.createCell(colIx++).setCellValue("Пол");
+            titleRow.createCell(colIx++).setCellValue("Дата рождения");
+            titleRow.createCell(colIx++).setCellValue("ИНН");
+            titleRow.createCell(colIx++).setCellValue("Индекс");
+            titleRow.createCell(colIx++).setCellValue("Страна");
+            titleRow.createCell(colIx++).setCellValue("Область");
+            titleRow.createCell(colIx++).setCellValue("Город");
+            titleRow.createCell(colIx++).setCellValue("Улица");
+            titleRow.createCell(colIx++).setCellValue("Дом");
+            titleRow.createCell(colIx++).setCellValue("Кв.");
 
-        DataFormat format = book.createDataFormat();
-        CellStyle innStyle = book.createCellStyle();
-        innStyle.setDataFormat(format.getFormat("#"));
-        for (int rowIx = 1; rowIx <= clients.length; rowIx++) {
-            Row row = sheet.createRow(rowIx);
-            colIx = 0;
-            row.createCell(colIx++).setCellValue(clients[rowIx-1].getName());
-            row.createCell(colIx++).setCellValue(clients[rowIx-1].getSurname());
-            row.createCell(colIx++).setCellValue(clients[rowIx-1].getPatronomic());
-            row.createCell(colIx++).setCellValue(clients[rowIx-1].getAge());
-            row.createCell(colIx++).setCellValue(clients[rowIx-1].getGender());
-            row.createCell(colIx++).setCellValue(clients[rowIx-1].getBD());
-            Cell innCell = row.createCell(colIx++);
-            innCell.setCellStyle(innStyle);
-            innCell.setCellValue(clients[rowIx-1].getInn());
-            row.createCell(colIx++).setCellValue(clients[rowIx-1].getIndex());
-            row.createCell(colIx++).setCellValue(clients[rowIx-1].getCountry());
-            row.createCell(colIx++).setCellValue(clients[rowIx-1].getRegion());
-            row.createCell(colIx++).setCellValue(clients[rowIx-1].getCity());
-            row.createCell(colIx++).setCellValue(clients[rowIx-1].getStreet());
-            row.createCell(colIx++).setCellValue(clients[rowIx-1].getHouse());
-            row.createCell(colIx++).setCellValue(clients[rowIx-1].getFlat());
+            DataFormat format = book.createDataFormat();
+            CellStyle innStyle = book.createCellStyle();
+            innStyle.setDataFormat(format.getFormat("#"));
+            for (int rowIx = 1; rowIx <= clients.length; rowIx++) {
+                Row row = sheet.createRow(rowIx);
+                colIx = 0;
+                row.createCell(colIx++).setCellValue(clients[rowIx - 1].getName());
+                row.createCell(colIx++).setCellValue(clients[rowIx - 1].getSurname());
+                row.createCell(colIx++).setCellValue(clients[rowIx - 1].getPatronomic());
+                row.createCell(colIx++).setCellValue(clients[rowIx - 1].getAge());
+                row.createCell(colIx++).setCellValue(clients[rowIx - 1].getGender());
+                row.createCell(colIx++).setCellValue(clients[rowIx - 1].getBD());
+                Cell innCell = row.createCell(colIx++);
+                innCell.setCellStyle(innStyle);
+                innCell.setCellValue(clients[rowIx - 1].getInn());
+                row.createCell(colIx++).setCellValue(clients[rowIx - 1].getIndex());
+                row.createCell(colIx++).setCellValue(clients[rowIx - 1].getCountry());
+                row.createCell(colIx++).setCellValue(clients[rowIx - 1].getRegion());
+                row.createCell(colIx++).setCellValue(clients[rowIx - 1].getCity());
+                row.createCell(colIx++).setCellValue(clients[rowIx - 1].getStreet());
+                row.createCell(colIx++).setCellValue(clients[rowIx - 1].getHouse());
+                row.createCell(colIx++).setCellValue(clients[rowIx - 1].getFlat());
+            }
+            for (colIx = 0; colIx < 14; colIx++) {
+                sheet.autoSizeColumn(colIx);
+            }
+            book.write(new FileOutputStream(EXCEL_FILE_NAME));
+            book.close();
+            System.out.println(new File(EXCEL_FILE_NAME).getAbsolutePath());
+        } catch (IOException ex) {
+            System.out.println("Could not write Excel file.");
         }
-        for (colIx = 0; colIx < 14; colIx++) {
-            sheet.autoSizeColumn(colIx);
-        }
-        book.write(new FileOutputStream(ExcelFileName));
-        book.close();
-        System.out.println(new File(ExcelFileName).getAbsolutePath());
     }
 
 }
